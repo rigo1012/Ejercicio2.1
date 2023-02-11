0be7b94c5adac071677dfbe896261e1cc8331845
rigo1012 <rodrigodomingo764@gmail.com>
2023-02-10 22:18:27 -0600

    Initial commit

---
+++ src/Operaciones.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Operaciones {
+    public static void main(String[]args){
+        Scanner dlt= new Scanner(System.in);
+        double num1, num2, suma, resta, mult
+        System.out.println("Operaciones");
+        System.out.println("Ingrese dos numeros");
+        num1 =dlt.nextDouble();
+        num2 = dlt.nextDouble();
+
+        //Operaciones
+        suma=num1+num2;
+        resta=num1+num2;
+        mult=num1*num2;
+
+
+    }
+}
